f52c7c64015cf42bd107187c9dab01c374bb40d1
ishynoris <mota.a.santos@gmail.com>
2018-07-13 07:04:19 -0300

    Adding all attributes of items / Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ interface IAnchors { icon: string, link: string, target: string }
 const Footer = (props: {}) => {
     const defaultColor = '#e7e7e7';
     const footer: React.CSSProperties = {
-        padding: '20px 0 20px 0',
+        padding: '30px 0 5px 0',
         textAlign: 'center'
     };
     const label: React.CSSProperties = {
@@ -17,28 +17,29 @@ const Footer = (props: {}) => {
         color: defaultColor,
         margin: '0 7px 0 7px',
     }
-    const anchors = ((ancs: IAnchors[]) => {
-        return ancs.map((a, i) => {
-            return <a key={i} style={icons} href={a.link} target={a.target}>
-                <i className={a.icon} />
-            </a>
-        })
-    })([
-        { icon: 'fab fa-github fa-lg', link: 'https://github.com/ishynoris/Priston-Calculator', target: '__blank' },
-        { icon: 'fab fa-facebook-f fa-lg', link: 'https://www.facebook.com/anailson.mota.7', target: '__blank' },
-        { icon: 'fas fa-envelope fa-lg', link: 'mailto:mota.a.santos@gmail.com?subject=Priston Calculator', target: '' },
-    ]);
 
     return (
         <div style={footer}>
             <label style={label}>
-                Encontrou algum erro? Quer sugerir alguma alteração? Entre em contato. <a style={icons}><i className={"fas fa-heart"} /></a>
+                Desenvolvido por: Anailson Santos Mota 
+                <i style={icons} className="fas fa-cog fa-spin fa-lg" />(v. alfa)
             </label>
-            <div>
-                {anchors}
+            <div> 
+                {((ancs: IAnchors[]) => {
+                    return ancs.map((a, i) => {
+                        return <a key={i} style={icons} href={a.link} target={a.target}>
+                            <i className={a.icon} />
+                        </a>
+                    })
+                })([
+                    { icon: 'fab fa-github fa-lg', link: 'https://github.com/ishynoris/Priston-Calculator', target: '__blank' },
+                    { icon: 'fab fa-facebook-f fa-lg', link: 'https://www.facebook.com/anailson.mota.7', target: '__blank' },
+                    { icon: 'fas fa-envelope fa-lg', link: 'mailto:mota.a.santos@gmail.com?subject=Priston Calculator', target: '' },
+                ])} 
             </div>
             <label style={label}>
-                Anailson Santos Mota<i style={icons} className="fas fa-cog fa-spin fa-lg" />(v. alfa)
+                Encontrou algum erro? Quer sugerir alguma alteração? Entre em contato. 
+                <a style={icons}><i className={"fas fa-heart"} /></a>
             </label>
         </div>
     )
