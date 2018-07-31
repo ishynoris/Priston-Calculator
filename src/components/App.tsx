1fb1cca2394c8f3cfb9b693e9f6beaf605df7116
ishynoris <mota.a.santos@gmail.com>
2018-07-31 15:09:28 -0300

    new branch to add multiples languages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,26 @@ import * as React from 'react';
 import IChar from '../interfaces/IChar';
 import CharDetail from './CharDetail';
 import Footer from './Footer';
+import SelectTitle from './SelectTitle';
 
 import '../assets/css/App.css';
 
 class App extends React.Component {
-
 	public render() {
+		
+		const langs: Array<{ option: string, value: string }> = [
+			{ option: "English", value: "en" },
+			{ option: "Português", value: "pt-br" }
+		]
+
 		return <div>
+			<div className="row">
+				<SelectTitle 
+					title="Multi-language"
+					name="mult-language"
+					values={langs}
+					/>
+			</div>
 			<CharDetail onCharChanged={this.charChanged}/>
 			<Footer />
 		</div>
