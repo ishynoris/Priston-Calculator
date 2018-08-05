880d16691f3504afcdd4d3fd89b9674ba577242c
ishynoris <mota.a.santos@gmail.com>
2018-08-05 02:41:16 -0300

    Some bugs afeter merge

--- src/interfaces/IChar.tsx
+++ src/interfaces/IChar.tsx
@@ -1,10 +1,8 @@
 import ICharacterStatus from './ICharacterStatus';
 import IFormula from './IFormula';
 import ISkills from './ISkills';
-// import IStatus from './IStatus';
 
 interface IChar {
-    // asSkills: (stats: ICharacterStatus) => IStatus[],
     formula: IFormula,
     name: string,
     skills: ISkills[],
