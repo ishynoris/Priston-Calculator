0f7c3de97b772dba3f00795361bba90ea6ec8f6a
ishynoris <mota.a.santos@gmail.com>
2019-03-27 00:46:29 -0300

    Version's date

--- src/assets/js/Versions.tsx
+++ src/assets/js/Versions.tsx
@@ -9,25 +9,30 @@ export default class Versions {
 
     public static description(version: IVersion): string {
         return "v" + version.v + (version.subV !== undefined ? "." + version.subV : "") 
-            + " - " + version.tag;
+            + " - " + version.tag
+            + " - " + version.date;
     }
 
 
     public static versions (): IVersion[] {
         return [
             {
+                date: "18/07/2018",
                 descriptions: ["Liberada todas as fórmulas para Arqueira, Sacerdotisa e Cavaleiro."],
                 tag: "alfa",
                 v: 0.1,
             }, {
+                date: "27/07/2018",
                 descriptions: ["Liberada as fórmulas de AR, DEF, ABS, HP, MP e RES para todos os personagens (incluindo Guerreira)."],
                 tag: "alfa",
                 v: 0.2,
             }, {
+                date: "28/07/2018",
                 descriptions: ["Adicionado as fórmulas de AP para Atalanta, Assassina e Guerreira."],
                 tag: "alfa",
                 v: 0.3,
             }, {
+                date: "29/07/2018",
                 descriptions: [
                     "Adicionado as fórmulas de AP para Mago e Lutador.", 
                     "A fórmula para Sacerdotisa foi corrigida."
@@ -36,14 +41,17 @@ export default class Versions {
                 tag: "alfa",
                 v: 0.3,
             }, {
+                date: "02/08/2018",
                 descriptions: [ "Adicionado seleção de idiomas." ],
                 tag: "alfa",
                 v: 0.4,
             }, {
+                date: "03/08/2018",
                 descriptions: [ "Notas de cada nova atualização." ],
                 tag: "alfa",
                 v: 0.5,
             }, {
+                date: "03/08/2018",
                 descriptions: [ 
                     "Correção de erro ao acessar Notas de atualização.",
                     "Novas traduções."
@@ -52,6 +60,7 @@ export default class Versions {
                 tag: "alfa",
                 v: 0.5,
             }, {
+                date: "05/08/2018",
                 descriptions: [ 
                     "Liberada as fórmulas de AP para e Mecânico, Pikeman e Xama.",
                     "Revisão geral das fórmulas dos personagens e correção de alguns erros.",
@@ -60,13 +69,20 @@ export default class Versions {
                 tag: "alfa",
                 v: 0.6,
             }, {
+                date: "10/08/2018",
                 descriptions: [ 
                     "Liberado o adicional dos boosters de Poder de Ataque",
                     "Liberado as fórmulas bases de todos os personagens."
                 ],
                 tag: "alfa",
                 v: 0.7,
-            },
+            }, {
+                date: "27/03/2019",
+                descriptions: [ "Data dos versionamentos." ],
+                subV: 1,
+                tag: "alfa",
+                v: 0.7,
+            }
         ]
     }
 }
--- src/interfaces/IVersion.tsx
+++ src/interfaces/IVersion.tsx
@@ -2,5 +2,6 @@ export default interface IVersion {
     tag: string;
     v: number;
     subV?: number;
+    date: string;
     descriptions: string[];
 }
